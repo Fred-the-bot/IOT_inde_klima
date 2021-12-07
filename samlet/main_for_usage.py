cb468fa6845d5e0de6a5c34c23d3ea90e9f9e11c
Technomancer <32704145+Fred-the-bot@users.noreply.github.com>
2021-12-07 11:45:55 +0100

    Update main_for_usage.py

--- samlet/main_for_usage.py
+++ samlet/main_for_usage.py
@@ -5,7 +5,6 @@
 #for at stoppe programmet skal man bare holde knappen der sidder i boksen inde i ca 6 sekunder
 
 
-from umqtt.simple import MQTTClient
 from machine import ADC, Pin, reset
 import time
 import math 
@@ -15,16 +14,29 @@ import urequests
 import ujson
 import secret
 import sys
+
 reset_button = Pin(13)
 adc = ADC(Pin(36)) #A4 på esp32
 adc.read()
 d = dht.DHT11(Pin(4))
+red_led = Pin(12, Pin.OUT)
+green_led = Pin(27, Pin.OUT)
+blue_led = Pin(33, Pin.OUT)
+red_led.off()
+green_led.off()
+blue_led.off()
+
 time.sleep(2) #der sættes en lille sleep på fra vi initialisere DHT sensoren da det gør systemet mere robust.
 
 i=0
 value=0
 j=0
 x=0
+
+red_led.off()
+green_led.off()
+blue_led.off()
+
 
 #https://stackoverflow.com/questions/2272149/round-to-5-or-other-number-in-python
 def myround(x, base=5):
@@ -48,7 +60,7 @@ def tabel(j):
 
 #forbinder sig til netværket med vores "hemmelige" wifi navn og "hemmelige" kode
 station = network.WLAN(network.STA_IF)
-station.active(True) 
+station.active(True)
 if not station.isconnected():
     station.connect(secret.ssid, secret.passwd)
 
@@ -65,12 +77,18 @@ temp = d.temperature()
 hum = d.humidity()
 
 while (i<5):
+     blue_led.on()
      if (reset_button.value() == 1):
+         blue_led.off()
+         red_led.on()
+         time.sleep(1)
+         red_led.off()
          sys.exit()
      value=adc.read()+value
      i=i+1
      time.sleep(0.5)
      if (i==5):
+         blue_led.off()
          d.measure()
          temp = d.temperature()
          hum = d.humidity()
@@ -85,15 +103,20 @@ while (i<5):
 
          upload = ujson.dumps({ 'Temp': temp, 'fugt': hum, 'lyd': loud }) #Tager og "omformatere" micropython tekster til brugbar JSON format
          if station.isconnected():
+             green_led.on()
              test = urequests.post("https://e36f7ff8-a58d-4076-b0aa-f7ffdc8efaf6.mock.pstmn.io", headers = {'content-type': 'application/json'}, data=upload) #poster vores tekst i formatet (IP,HEADER,DATA)
              time.sleep(10)#giver programmet 10 sekunder til at udføre sin POST før forbindelsen lukkes
              test.close() #lukker forbindelsen efter hvert POST da den ellers vil lave "memory overflow"
+             green_led.off()
          else:
              station.connect(secret.ssid, secret.passwd)
          i=0
          while(i<100):
              i=i+1
              if (reset_button.value() == 1):
+                  red_led.on()
+                  time.sleep(1)
+                  red_led.off()
                   sys.exit()
              time.sleep(6)
          reset()
