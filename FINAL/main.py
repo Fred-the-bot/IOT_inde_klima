6ec86489a0c540b420129441e18cc664db98955b
Technomancer <32704145+Fred-the-bot@users.noreply.github.com>
2022-01-14 22:00:54 +0100

    Update main.py

--- FINAL/main.py
+++ FINAL/main.py
@@ -38,7 +38,7 @@ red_led.off()
 green_led.off()
 blue_led.off()
 
-#ntptime.host = "1.europe.pool.ntp.org"
+ntptime.host = "1.europe.pool.ntp.org"
 
 #https://stackoverflow.com/questions/2272149/round-to-5-or-other-number-in-python
 def myround(x, base=5):
@@ -63,18 +63,14 @@ def tabel(j):
 #forbinder sig til netværket med vores "hemmelige" wifi navn og "hemmelige" kode
 station = network.WLAN(network.STA_IF)
 station.active(True)
-#while not station.isconnected():
-    #station.connect(secret.ssid, secret.passwd)
-    #time.sleep(2)
+while not station.isconnected():
+    station.connect(secret.ssid, secret.passwd)
+    time.sleep(2)
 
 
 
-#ntptime.settime() denne sætter tiden korrekt efter reset (da de kan resette tiden) pt. udkommenteret da den ikke kommer på WAN, men istedet i lukket lan.
+ntptime.settime() #denne sætter tiden korrekt efter reset (da de kan resette tiden) 
 SERVER = secret.IP  #  Server Address 
-
-
-
-#bruges ikke, men kan tilføjes for at sætte flere enheder på
 apiKeyValue = secret.api
 sensorName = str("ESP32")
 sensorLocation = str("Lokale1")
@@ -83,9 +79,9 @@ temp = d.temperature()
 hum = d.humidity()
 print(time.localtime())
 while (i<5):
-     #if time.localtime()[3]+1>18 or time.localtime()[3]+1<5:
-      #   time.sleep(3600)
-       #  reset()
+     if time.localtime()[3]+1>18 or time.localtime()[3]+1<5:
+         time.sleep(3600)
+         reset()
          
      blue_led.on()
      if (reset_button.value() == 1):
@@ -127,15 +123,9 @@ while (i<5):
          if station.isconnected():
              green_led.on()
              test = urequests.post(SERVER, headers = {'content-type': 'application/x-www-form-urlencoded'}, data="api_key=" + apiKeyValue + "&sensor=" + sensorName + "&location=" + sensorLocation + "&value1=" + temp + "&value2=" + hum + "&value3=" + lyd) #poster vores tekst i formatet (IP,HEADER,DATA)
-             time.sleep(5)#giver programmet 5 sekunder til at udføre sin POST før forbindelsen lukkes
-             #ntptime.settime()
+             time.sleep(5) #giver programmet 5 sekunder til at udføre sin POST før forbindelsen lukkes
+             ntptime.settime()
              green_led.off()
-             print(temp)
-             print(type(temp))
-             print(hum)
-             print(type(hum))
-             print(lyd)
-             print(type(lyd))
          else:
              print("ikke forbundet")
              station.connect(secret.ssid, secret.passwd)
